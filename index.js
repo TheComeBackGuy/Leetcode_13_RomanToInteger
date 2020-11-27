71601fa15681b258a6d6cc7459858693f4bcfa62
TheComeBackGuy <71109038+TheComeBackGuy@users.noreply.github.com>
2020-11-27 17:13:42 -0500

    Not quite there
    Still trying to solve the exception of having IV vs VI. 
    adding normal values was relatively easy.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var romanToInt = function (s) {
   let score = 0;
   //split input to array to use array functions
   let romArray = s.split("");
+  console.log(romArray);
 
   const Romans = [
     ["I", 1],
@@ -19,23 +20,27 @@ var romanToInt = function (s) {
     ["M", 1000],
     []
   ];
-  // console.log(Romans[0][0]);
+  // console.log(Romans[6][0].match(/M/gi));
   romArray.forEach((romNum) => {
-    //really tried not to use a for loop, but... needed to cycle through values
-    //and didn't know any other way to.
+    //really tried not to use a for loop, but... needed to
+    //incrementally cycle through values and didn't see any other way to.
     for (let i = 0; i < Romans.length; i++) {
-      /**
-       * What's wanted is if(romArray value matches corresponding Romans name AND
-       * there's not a Item with more value than itself in Romans IN the rest of romArray
-       * Then add it's value. If there is, subtract that value. )
-       */
-      if (romNum === Romans[i][0]) {
-        console.log(`Superior of ${romNum} is ${Romans[i + 1][0]}`);
-        console.log(`Next item after ${romNum} is ${Object.keys(romNum)}`);
-
-        // console.log(Romans[i][0] + " + " + s.indexOf(i) + " + " + s.length);
+      if (
+        //if current string value === a value in Romans array
+        romNum === Romans[i][0] &&
+        // AND there are no instances of the next largest value
+        //from the starting point of the current romArray index
+        romArray.indexOf(Romans[i + 1][0], romArray.indexOf(romArray)) === -1
+      ) {
+        //score is increased by the value of that item
         score += Romans[i][1];
-      } else if (romNum === Romans[i][0] && s.indexOf(romNum) < s.indexOf(i)) {
+      } else if (
+        //if current string value === a value in Romans array
+        romNum === Romans[i][0] &&
+        //AND if an instance of the next largest value is found
+        romArray.indexOf(Romans[i + 1][0], romArray.indexOf(romArray)) !== -1
+      ) {
+        // score is decreased by the value of that item
         score -= Romans[i][1];
       }
     }
@@ -46,4 +51,4 @@ var romanToInt = function (s) {
   return score;
 };
 ///tesable vall to function
-romanToInt("MD");
+romanToInt("MCM");
