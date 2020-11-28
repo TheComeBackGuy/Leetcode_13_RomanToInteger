84bbe751836bf18a4844223ed76104f98558324b
TheComeBackGuy <71109038+TheComeBackGuy@users.noreply.github.com>
2020-11-28 17:35:37 -0500

    Passed code
    Instead of trying to write something that detected the rest of the string, I simply hard-coded the acceptable values for Roman Numerals in.
    
    future tests could be to detect a correctly submitted Roman Numeral and only converting it if that passes.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 //established function for excersize
 var romanToInt = function (s) {
-  if (s.length < 1 || s.length >= 15) {
+  if (s.length < 1 || s.length > 15) {
     return;
   }
   let newTotal = 0;
@@ -13,7 +13,7 @@ var romanToInt = function (s) {
   // let score = 0;
   //split input to array to use array functions
   let romArray = s.split("");
-  console.log(romArray);
+  console.log("romArray is : " + romArray);
 
   const romans = [
     ["I", 1],
@@ -24,38 +24,40 @@ var romanToInt = function (s) {
     ["D", 500],
     ["M", 1000]
   ];
-  // console.log(Romans[6][0].match(/M/gi));
+
   romArray.forEach((romNum) => {
-    //make new array with nujmber values
     for (let i = 0; i < romans.length; i++) {
       if (romNum === romans[i][0]) {
         intArray.push(romans[i][1]);
-        // intArray.reduce(addItUp);
-        // console.log(romans[i][1]);
       }
     }
-    for (let j = 0; j < intArray.length; j++) {
-      if (intArray[j + 1] > intArray[j]) {
-        intArray[j] *= -1;
-        console.log("J is : " + intArray[j]);
-        console.log(intArray[j] + 1);
-      }
-    }
-
     console.log(intArray);
-    let addItUp = (total, addOn) => total + addOn;
-
-    newTotal = intArray.reduce(addItUp);
-    console.log(newTotal);
   });
+
+  for (let j = 0; j < intArray.length; j++) {
+    if (
+      intArray[j] < intArray[j + 1] ||
+      intArray[j] < intArray[j + 2] ||
+      intArray[j] < intArray[j + 3]
+    ) {
+      intArray[j] *= -1;
+      // score += romans[j][1];
+    }
+    // score -= r
+
+    //item > J's value
+    // intArray.includes()
+    // console.log("yup")
+  }
+  let addItUp = (total, addOn) => total + addOn;
+  newTotal = intArray.reduce(addItUp);
+  // for (j=0;j<intArray.length){
+  console.log(newTotal);
   return newTotal;
+  // }
 };
 
-//return the score for example
-// console.log(score);
-// return score;
-
 ///tesable vall to function
-romanToInt("MCDL");
+romanToInt("MCDLXXVI");
 
 //MCDLXXVI
